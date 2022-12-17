725a5c3ae4d57b5d2ba25e52a9e2d593a2415ce0
Henrique Silva Coutinho <92997597+Hesico@users.noreply.github.com>
2022-12-17 15:11:44 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel(val peso : Int) { 
+    BASICO(1), 
+    INTERMEDIARIO(2), 
+    AVANCADO(3) }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome : String);
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val nivel : Nivel, val duracao: Int = 60){
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    // De lista para Set pois um mesmo usuário não pode estar duas vezes matriculado no mesmo curso
+    val inscritos = mutableSetOf<Usuario>();
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    //Nivel da formação =  maior nível dos conteúdos
+    val nivel : Nivel
+    	get() { return conteudos.maxByOrNull { it.nivel.peso }?.nivel ?: Nivel.BASICO }
+    
+    val duracao : Int
+    	get() { return conteudos.map {it.duracao}.sum() }
+    
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach{
+            inscritos.add(it);
+        	println("${it.nome} foi matriculado na formação ${nome}")
+        }   
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+	val modulo1 = ConteudoEducacional("Criando um projeto em React", Nivel.BASICO)
+	val modulo2 = ConteudoEducacional("Criando compentes efetivos com useEffect", Nivel.INTERMEDIARIO, 90)
+    val formacaoReact = Formacao("React", listOf(modulo1,modulo2));
+    
+    val user1 = Usuario("Henrique");
+    val user2 = Usuario("Francisco");
+    val user3 = Usuario("Wedma");
+    
+    formacaoReact.matricular(user1);
+    formacaoReact.matricular(user2, user3);
+    
+    println(formacaoReact.inscritos);
+    println(formacaoReact.nivel);
+    println(formacaoReact.duracao);
 }
